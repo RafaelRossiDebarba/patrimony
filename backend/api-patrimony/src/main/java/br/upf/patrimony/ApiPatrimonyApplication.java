7edc5d765379ad9735619d07b811bd52b965e5f8
Rafael Rossi Debarba <rafadeross@hotmail.com>
2022-03-14 20:28:03 -0300

    Backend patrimony

---
+++ backend/api-patrimony/src/main/java/br/upf/patrimony/ApiPatrimonyApplication.java
@@ -0,0 +1,13 @@
+package br.upf.patrimony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPatrimonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPatrimonyApplication.class, args);
+	}
+
+}
---
+++ backend/api-patrimony/src/test/java/br/upf/patrimony/ApiPatrimonyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upf.patrimony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPatrimonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
